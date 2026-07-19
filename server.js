c147fa7b94f1fc332264e4340b54cef5257842c6
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-19 18:06:24 +0800

    Creating conditions to only send responses

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ import http from "node:http"
 const PORT = 8000
 
 const server = http.createServer((request, response) => {
-  response.end("Hello from the server!")
+  const urlProperty = request.url
+  const requestMethod = request.method
+
+  if (urlProperty === "/api" && requestMethod === "GET") {
+    response.end(
+      "Hello from the server!", 
+      "utf8", 
+      () => console.log("response end")
+    )
+  }
 })
 
-server.listen(PORT, () => console.log(`server running at port: ${PORT}`))
+server.listen(PORT, () => console.log(`server running at port ${PORT}`))
