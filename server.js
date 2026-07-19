0bcfcf4ca84fa0d6170ef6a1904a2886d6436605
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-19 15:16:16 +0800

    Built a simple server and a get request

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello Node!")
+import http from "node:http"
+
+const PORT = 8000
+
+const server = http.createServer((request, response) => {
+  response.end("Hello from the server!")
+})
+
+server.listen(PORT, () => console.log(`server running at port: ${PORT}`))
