9e454b6bc46c6f6df2735e86b4571b280fd0484a
Muelvin Lopez <muelvinlopez25@gmail.com>
2026-07-19 18:52:23 +0800

    Sending json data to the endpoint

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import http from "node:http"
+import { getDataFromDb } from "./database/db.js"
 
 const PORT = 8000
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const urlProperty = request.url
   const requestMethod = request.method
+  const destination = await getDataFromDb()
 
   if (urlProperty === "/api" && requestMethod === "GET") {
+    const stringifiedDestination = JSON.stringify(destination)
+
+    response.write(stringifiedDestination)
+    response.write("\n")
     response.end(
       "Hello from the server!", 
       "utf8", 
@@ -15,4 +21,4 @@ const server = http.createServer((request, response) => {
   }
 })
 
-server.listen(PORT, () => console.log(`server running at port ${PORT}`))
+server.listen(PORT, () => console.log(`server running at port ${ PORT }`))
